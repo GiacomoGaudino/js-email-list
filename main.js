87af29095d3546ee07809590025d8aa226b981cb
GiacomoGaudino <giacomo.gaudino@yahoo.it>
2025-08-02 15:47:32 +0200

    add html markup, js function and img

--- main.js
+++ main.js
@@ -1,12 +1,23 @@
 console.log("hi there");
+const emailEl = document.getElementById("email");
+const refreshEl = document.getElementById("refresh");
+const randomEmailEndpoint = "https://flynn.boolean.careers/exercises/api/random/mail";
 
-const randomEmailEndpoint = "https://flynn.boolean.careers/exercises/api/random/mail"
-for (let i = 0; i < 10; i++) {
-    axios.get(randomEmailEndpoint)
-        .then(response => {
-            const radomEmail = response.data.response
-            console.log(radomEmail);
-        })
+function loadEmail() {
+    emailEl.innerHTML = "";
+    for (let i = 0; i < 10; i++) {
+        axios.get(randomEmailEndpoint)
+            .then(response => {
+                const radomEmail = response.data.response
+                console.log(radomEmail);
+                const li = document.createElement("li")
+                li.classList.add("list-group-item", "text-info-emphasis", "email-item")
+                li.innerHTML = radomEmail
+                emailEl.appendChild(li)
+            })
+    }
 }
 
+loadEmail();
 
+refreshEl.addEventListener('click', () => { loadEmail() })
